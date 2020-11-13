e4c5405a08010176bc856ffd790f0ca7c959790c
Michael Iodice <miodice3@gmail.com>
2020-11-13 11:01:28 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,62 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    #binding.pry
+    @recipes = Recipe.create(params)
+    redirect to "/recipes/#{@recipes.id}"
+  end
+
+  get '/recipes/:id' do
+    
+    @recipes = Recipe.find_by(id: params[:id])
+    #binding.pry
+    if @recipes
+      erb :show
+    else
+      "whoops that recipe doesnt exist!"
+    end
+  end
+
+  get '/recipes/:id/edit' do
+    @recipes = Recipe.find_by(id: params[:id])
+    #binding.pry
+    if @recipes
+      erb :edit
+    else
+      "whoops that recipe doesnt exist!"
+    end
+  end
+
+  patch '/recipes/:id' do
+    #binding.pry
+    recipe=Recipe.find_by_id(params[:id])
+    recipe.update(params[:update])
+    #binding.pry
+
+    redirect "/recipes/#{recipe.id}"
+  end
+
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    #binding.pry
+    erb :index
+  end
+
+  delete '/recipes/:id' do
+    @recipes = Recipe.find_by(id: params[:id])
+    #binding.pry
+    @recipes.destroy
+    redirect "/recipes"
+  end
 
 end
